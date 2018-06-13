6bf19d6ffbd79efe7b69d66000dc8f26ebef52cf
humstarman <lueyoung7@gmail.com>
2018-06-13 16:12:22 +0800

    update: fix bug

--- update-pem/clear-expired-pem.sh
+++ update-pem/clear-expired-pem.sh
@@ -2,6 +2,8 @@
 
 DIRS="/etc/kubernetes/ssl /etc/flanneld/ssl /etc/etcd/ssl ./ssl"
 for DIR in $DIRS; do
-  [[ -d "$DIR" && '/' != "$DIR" ]] && rm -rf $DIR/* && \
-    echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [WARN] - clear $DIR ... "
+  if [[ -d "$DIR" && '/' != "$DIR" ]]; then
+    rm -rf $DIR/*
+    echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [WARN] - clear $DIR"
+  fi
 done
