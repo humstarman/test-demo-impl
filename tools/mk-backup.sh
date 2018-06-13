7ec374e2e58d8da3bc968c1cce95185d3c8fb0e4
humstarman <lueyoung7@gmail.com>
2018-06-13 14:17:42 +0800

    update

--- tools/mk-backup.sh
+++ tools/mk-backup.sh
@@ -2,7 +2,7 @@
 
 BAK=/var/k8s/bak
 NOW=$(date -d today +'%Y-%m-%d-%H:%M:%S')
-ansible master -m shell -a "[ -d ${BAK} ] && mv $BAK ${BAK}-${NOW}"
+ansible master -m shell -a "[ -d "${BAK}" ] && mv $BAK ${BAK}-${NOW}"
 ansible master -m shell -a "mkdir -p $BAK"
 #THIS_DIR=$(cd "$(dirname "$0")";pwd)
 TMP=/tmp/k8s/bak
