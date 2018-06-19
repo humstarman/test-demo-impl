49f47ff1cfc25f902e4ccb4f225680b4849099b3
humstarman <lueyoung7@gmail.com>
2018-06-19 10:46:15 +0800

    update: info

--- add-master/cp-kubernetes-master-components.sh
+++ add-master/cp-kubernetes-master-components.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - cp Kubernetes node components ... "
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - cp Kubernetes master & node components ... "
 BIN=/usr/local/bin
 TMP=/tmp/k8s-node-components
 [ -d "$TMP" ] && rm -rf $TMP
@@ -9,5 +9,5 @@ cd $BIN && \
   cp kube-apiserver kube-controller-manager kube-scheduler kubelet kube-proxy kubectl $TMP && \
   cd - 
 ansible new -m copy -a "src=${TMP}/ dest=$BIN mode='a+x'"
-echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - Kubernetes node components distributed."
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - Kubernetes master & node components distributed."
 exit 0
