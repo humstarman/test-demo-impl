a35de01f0260516fe6a127757cf880a6b3942626
humstarman <lueyoung7@gmail.com>
2018-07-09 10:26:17 +0800

    update

--- tools/update-ansible-hosts.sh
+++ tools/update-ansible-hosts.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 set -e
-ANSIBLE=/etc/ansible/hosts
-[ -f "$ANSIBLE" ] && rm -f $ANSIBLE
-[ -f "$ANSIBLE" ] || touch $ANSIBLE
-CSVS=$(ls | grep -E ".csv$")
-for CSV in $CSVS; do
-  GROUP=$CSV
-  GROUP=${GROUP##*/}
-  GROUP=${GROUP%.*}
-  cat >> $ANSIBLE << EOF 
-[$GROUP]
-EOF
-  MEMBERS=$(sed s/","/" "/g $CSV)
-  for MEMBER in $MEMBERS; do
-    echo $MEMBER >> $ANSIBLE
+FILE=info.env
+if [ -f ./$FILE ]; then
+  source ./$FILE
+else
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - no environment file found!" 
+  echo " - exit!"
+  sleep 3
+  exit 1
+fi
+TOOLS=$URL/tools
+getScript () {
+  TRY=10
+  URL=$1
+  SCRIPT=$2
+  for i in $(seq -s " " 1 ${TRY}); do
+    curl -s -o ./$SCRIPT $URL/$SCRIPT
+    if cat ./$SCRIPT | grep "404: Not Found"; then
+      rm -f ./$SCRIPT
+    else
+      break
+    fi
   done
-  echo "" >> $ANSIBLE
-done
-
+  if [ -f "./$SCRIPT" ]; then
+    chmod +x ./$SCRIPT
+  else
+    echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - downloading failed !!!" 
+    echo " - $URL/$SCRIPT"
+    echo " - Please check !!!"
+    sleep 3
+    exit 1
+  fi
+}
+getScript $TOOLS mk-ansible-hosts.sh
+ANSIBLE=/etc/ansible/hosts
+CSVS=$(ls | grep -E ".csv$")
+if [ -n "$CSVS" ]; then
+  for CSV in $CSVS; do
+    GROUP=$CSV
+    GROUP=${GROUP##*/}
+    GROUP=${GROUP%.*}
+    ./mk-ansible-hosts.sh -g $GROUP -i $(cat $CSV) -a $ANSIBLE -o
+  done
+fi
